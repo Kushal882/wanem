0bdab4c71c7c231e9c20048ab75c3cee0d67446d
Kushal882 <kushallimbu365@gmail.com>
2026-06-09 10:24:23 +0900

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,25 @@
+const {contextBridge, ipcRenderer} = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    // File import/export and dialogs
+    saveAs: (text) => ipcRenderer.invoke('save-as', text),
+    openFile: () => ipcRenderer.invoke('open-file'),
+    newNote: () => ipcRenderer.invoke('new-note'),
+
+    // Menu event listeners (main -> renderer)
+    onMenuNewNote: (callback) => ipcRenderer.on('menu-new-note', callback),
+    onMenuOpenFile: (callback) => ipcRenderer.on('menu-open-file', callback),
+    onMenuSaveNote: (callback) => ipcRenderer.on('menu-save-note', callback),
+    onMenuSaveAs: (callback) => ipcRenderer.on('menu-save-as', callback),
+    onMenuDeleteNote: (callback) => ipcRenderer.on('menu-delete-note', callback),
+
+    // JSON storage APIs
+    getNotes: () => ipcRenderer.invoke('get-notes'),
+    deleteNoteJson: (id) => ipcRenderer.invoke('delete-note-json', id),
+    saveNoteJson: (note) => ipcRenderer.invoke('save-note-json', note),
+    getSettings: () => ipcRenderer.invoke('get-settings'),
+    saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
+    showNotification: (notification) => ipcRenderer.invoke('show-notification', notification),
+    togglePin: (id) => ipcRenderer.invoke('toggle-pin', id)
+
+});
